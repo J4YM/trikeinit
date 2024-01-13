a9afed8737ffb2c1c7f026a8655fa0f9b5757bc5
EruRoraito <joshcruz2028@gmail.com>
2024-01-14 03:33:22 +0800

    dsads

---
+++ ConditionalRender.js
@@ -0,0 +1,20 @@
+function showBookElement() 
+{
+    var BookElement = document.getElementById("BookElement");
+    if (BookElement.style.display === "block") 
+    {
+      BookElement.style.display = "none";
+    } 
+    else 
+    {
+      BookElement.style.display = "block";
+    }
+
+    console.log(BookElement);
+}
+
+
+function BookRide()
+{
+
+}
